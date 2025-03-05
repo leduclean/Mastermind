5ff6f59565da3518d0c6cd7864010970150a851b
Simon <simonvanbommel@gmail.com>
2025-03-05 10:55:58 +0100

    chore(i18n): translate variables to English

--- common.py
+++ common.py
@@ -11,58 +11,58 @@ def evaluation(arg, ref):
     assert len(arg) == len(ref), "Les deux combinaisons doivent avoir la même longueur"
 
     LENGTH = len(arg)
-    bien_places = 0
-    mal_places = 0
+    correctly_placed = 0
+    incorrectly_placed = 0
 
     # Étape 1 : Détection des bien placés
-    reste_arg = []
-    reste_ref = []
+    rest_arg = []
+    rest_ref = []
 
     for i in range(LENGTH):
         # assert arg[i] in COLORS, "Les couleurs en entré doivent être dans les couleurs disponibles"
         if arg[i] == ref[i]:
-            bien_places += 1
+            correctly_placed += 1
         else:
-            reste_arg.append(arg[i])
-            reste_ref.append(ref[i])
+            rest_arg.append(arg[i])
+            rest_ref.append(ref[i])
 
     # Étape 2 : Détection des mal placés (présents mais mal placés)
-    for couleur in reste_arg:
-        if couleur in reste_ref:
-            mal_places += 1
-            reste_ref.remove(couleur)  # Empêche de compter une couleur plusieurs fois
+    for color in rest_arg:
+        if color in rest_ref:
+            incorrectly_placed += 1
+            rest_ref.remove(color)  # Empêche de compter une couleur plusieurs fois
 
-    return bien_places, mal_places
+    return correctly_placed , incorrectly_placed
 
 
-#TO DO : Ajouter doc string et meilleur commentaires et tt ET CHANGER LES VARIABLES EN ANGLAIS
+#TO DO : Ajouter doc string et meilleur commentaires et tt
 
-def donner_possibles(combinaison_teste,evaluation_associe):
-    bien_places,mal_places=evaluation_associe
+def donner_possibles(tested_combination, associated_evaluation):
+    correctly_placed , incorrectly_placed = associated_evaluation
     #Faire un ensemble avec toutes les possibilites,
-    liste_ensemble_permutation=itertools.permutations(COLORS,LENGTH)
+    permutation_list=itertools.permutations(COLORS,LENGTH)
     #Maintenant on va supprimer les combinaisons qui sont pas possibles et apres on aura l ensemble des combinaisons finales
 
 
-    liste_return = []
+    return_list = []
 
 
-    for element in liste_ensemble_permutation :
+    for element in permutation_list :
         #On regarde si le nombre de meme couleur, si c'est egal a bien_places+ mal_places c'est deja bien
         #Ensuite on regarde si le nombre de meme places = nombre bien places
-        bplaces,mplaces = evaluation(element,combinaison_teste)
-        if bplaces == bien_places and mplaces == mal_places :
-            liste_return.append(element)
+        cplaces,iplaces = evaluation(element,tested_combination)
+        if cplaces == correctly_placed and iplaces == incorrectly_placed :
+            return_list.append(element)
 
     #print(liste_return)
-    return set(liste_return)
+    return set(return_list)
 
-# To DO : Commentaires + changer les variables en anglais
-def maj_possibles(ensemble_combinaisons_possibles, combinaison_teste, evaluation_associe):
-    ensemble_combinaisons_possibles_nouveau = donner_possibles(combinaison_teste,evaluation_associe)
+# To DO : Commentaires
+def maj_possibles(possible_combinations, tested_combination, associated_evaluation):
+    new_possible_combinations = donner_possibles(tested_combination,associated_evaluation)
 
-    ensemble_combinaisons_possibles.intersection_update(ensemble_combinaisons_possibles_nouveau) # mets a jour directement le set avant le .intersectionupdate
-    return ensemble_combinaisons_possibles
+    possible_combinations.intersection_update(new_possible_combinations) # mets a jour directement le set avant le .intersectionupdate
+    return possible_combinations
 
 
 #%% Partie Test

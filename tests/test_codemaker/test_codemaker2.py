859b9b64d91fe96a2b9c904e9ee27569a6cb0fce
LeDuc <leandre.leduc38@gmail.com>
2025-03-23 15:13:25 +0100

    test(codemaker): update codemaker tests

--- tests/test_codemaker/test_codemaker2.py
+++ tests/test_codemaker/test_codemaker2.py
@@ -3,6 +3,7 @@ import app.mastermind.codemaker2 as codemaker2
 import app.mastermind.common as common
 import app.mastermind.past_evaluations as past_evaluations
 
+# To do créer un jeu de test en entré dont on connais la sortie
 
 class TestCodemaker2(unittest.TestCase):
 
@@ -11,8 +12,8 @@ class TestCodemaker2(unittest.TestCase):
         Initialise les variables globales avant chaque test.
         """
         codemaker2.solution = "RRRR"
-        codemaker2.possible_combinations = {"RRRR", "VVVV", "VVRR"}
-        codemaker2.permanent_combinations = {"RRRR", "VVVV", "VVRR", "BBBB"}
+        codemaker2.possible_combinations = {"RRRR", "VVBB", "VBBR", "BBVV", "VRBB", "VVVV"}
+        codemaker2.permanent_combinations = codemaker2.possible_combinations.copy()
         past_evaluations.dict_backtracking = {}
 
     def test_exact_match(self):
@@ -45,19 +46,8 @@ class TestCodemaker2(unittest.TestCase):
         # La nouvelle solution doit appartenir à l'ensemble des solutions possibles
         self.assertIn(codemaker2.solution, codemaker2.possible_combinations,
                       "Erreur: la solution mise à jour n'est pas dans les possibilités.")
-
-        # Vérifier que la nouvelle solution maximise le pire cas (simulation)
-        worst_case_sizes = []
-        for test_combination in codemaker2.possible_combinations:
-            evaluation_groups = {}
-            for comb in codemaker2.permanent_combinations:
-                eval_result = common.evaluation(test_combination, comb)
-                if eval_result not in evaluation_groups:
-                    evaluation_groups[eval_result] = []
-                evaluation_groups[eval_result].append(comb)
-            worst_case_sizes.append(max(len(group) for group in evaluation_groups.values()))
-
-        self.assertEqual(codemaker2.solution, max(codemaker2.possible_combinations, key=lambda x: worst_case_sizes),
+        # La nouvelle solution doit etre la plus dure a trouver, parmis les possible combination, elle doit etre dans le groupe avec le plus d'ambiguité donc dans {RRRR, VVBB, VBBR, VVVV}
+        self.assertIn(codemaker2.solution, {"RRRR", "VVBB", "VBBR", "VVVV"},
                          "Erreur: la solution mise à jour n'est pas optimisée pour maximiser la difficulté.")
 
     def test_evaluation_storage(self):
